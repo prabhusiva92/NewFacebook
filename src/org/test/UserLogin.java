fb45b2bd945c2b38e868ca769a4399e251e22d24
prabhusiva92 <sjprabhu91@gmail.com>
2022-01-01 10:14:37 +0530

    Folder Structure

---
+++ src/org/test/UserLogin.java
@@ -0,0 +1,12 @@
+package org.test;
+
+public class UserLogin {
+
+	private void login() {
+		System.out.println("login page");
+	}
+	public static void main(String[] args) {
+	UserLogin lg = new UserLogin();
+		lg.login();	
+	}
+}
